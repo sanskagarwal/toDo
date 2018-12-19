d4896fe700ed59f07a5cb32dcd97d44fa6aeb16b
Sanskar <sanskaragarwal.500@gmail.com>
2018-12-19 14:31:50 +0530

    Added all todos routes

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser'); 
-var {ObjectID} = require('mongodb');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser'); 
+const {ObjectID} = require('mongodb');
+const _ = require("lodash");
 
-var {mongoose} = require('./db/mongoose.js');
-var {Todo} = require('./models/Todo.js');
-var {User} = require('./models/User.js');
+const {mongoose} = require('./db/mongoose.js');
+const {Todo} = require('./models/Todo.js');
+const {User} = require('./models/User.js');
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
@@ -17,7 +18,7 @@ app.post('/todos', (req,res) => {
 
     todo.save().then((todo) => {
         res.send(todo);
-    },(err) => {
+    }).catch((err) => {
         res.status(400).send(err);
     });
 });
@@ -25,7 +26,7 @@ app.post('/todos', (req,res) => {
 app.get('/todos', (req,res) => {
     Todo.find({}).then((todos) => {
         res.send({todos})
-    }, (err) => {
+    }).catch((err) => {
         res.status(400).send(err);
     });
 });
@@ -40,8 +41,46 @@ app.get('/todos/:id', (req,res) => {
             return res.status(400).send({error: "User ID not found"});
         }
         res.send({todo});
-    }, (err) => {
+    }).catch((err) => {
         res.status(400).send(err);
+    });
+});
+
+app.delete('/todos/:id', (req,res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send({error: "ID not valid"});
+    }
+    Todo.findOneAndDelete({_id:id}).then((todo) => {
+        if(!todo) {
+            return res.status(400).send({error: "User ID not found"});
+        }
+        res.send({todo});
+    }).catch((err) => {
+        res.status(400).send(err);
+    });
+});
+
+app.patch("/todos/:id", (req,res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body,["text","completed"]);
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send({error: "ID not valid"});
+    }
+    if(_.isBoolean(body.completed) && body.completed==true) {
+        body.completedAt = new Date().getTime();
+    } else if(_.isBoolean(body.completed) && body.completed==false) {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findOneAndUpdate({_id: id},{$set: body},{new:true}).then((todo) => {
+        if(!todo) {
+            return res.status(404).send({error: "User ID not found"});
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send(e);
     });
 });
 
--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 var URI = 'mongodb://skbro:sk1234@ds123136.mlab.com:23136/todo-app';
 mongoose.Promise = global.Promise;
-mongoose.connect(URI, {useNewUrlParser: true});
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.connect(URI);
 
 module.exports = {mongoose};
